f325fff701e7b72c20f6327c0128711e2838b40c
Your Name <amasarfowaap@gmail.com>
2024-12-13 11:23:44 +0000

    adding changes

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// let x = prompt ("give me a number");
+// console.log(typeof(x));
+// x = number(x);
+// console.log(typeof(x));
+
+// let results = x + 15;
+// let reminder = results % 2;
+
+// if (reminder == 1){
+//   alert("winner")
+// }else{
+//     ("loser");
+// }
+
+   //if you want to prompt a user to delete or not
+let del = prompt("Are u sure u want to delete?");
+console.log(del);
+
+if(del ==="yes"){
+    alert("deletion successful");
+}else {
+    alert("deletion unsuccessful");
+}
+
+// let booking = prompt ("let us know if you will like to cancel, confirm or rescheduled your appointment");
+// if (booking == "confirm"){
+//     alert ("see you soon");
+// } else if (booking == "cancelled"){
+//     alert (`booking cancelled`);
+// }else if (booking =="lets rescheduled") = prompt (" when would u like to rescheduled to"){
+//     alert (`your appointment has been rescheduled to + rescheduled`);
+// }else {
+//     alert(`please cancel, confirm or rescheduled`);
+// }
+
+
+
